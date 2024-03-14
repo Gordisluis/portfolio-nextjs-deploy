81b1be50ea856040ffac485d7a0fb63815a44291
luis garcia <vpedev@gmail.com>
2024-03-14 12:11:52 -0400

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import TransitionProvider from "@/components/transitionProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "VpeDev - Portfolio",
+  description: "el mejor portfolio de programador de la ciudad",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        <TransitionProvider>{children}</TransitionProvider>
+      </body>
+    </html>
+  );
+}
